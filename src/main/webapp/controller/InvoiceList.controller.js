e12cf58021de7e870d200abdfc105bb9c5a12c72
monssefa <monssef.alsweis@gmail.com>
2017-02-10 09:40:17 +0100

    new file jsp

---
+++ src/main/webapp/controller/InvoiceList.controller.js
@@ -0,0 +1,42 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
+        "sap/ui/demo/wt/model/formatter",
+        "sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function (Controller, JSONModel, formatter, Filter, FilterOperator) {
+	"use strict";
+
+	return Controller.extend("sap.ui.demo.wt.controller.InvoiceList", {
+                formatterr: formatter,  
+		onInit : function () {
+			var oViewModel = new JSONModel({
+				currency: "EUR"
+			});
+			this.getView().setModel(oViewModel, "view");
+		},
+               onFilterInvoices : function (oEvent) {
+
+			// build filter array
+			var aFilter = [];
+			var sQuery = oEvent.getParameter("query");
+			if (sQuery) {
+				aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+			}
+
+			// filter binding
+			var oList = this.getView().byId("invoiceList");
+			var oBinding = oList.getBinding("items");
+			oBinding.filter(aFilter);
+		} 
+
+	});
+});
+
+
